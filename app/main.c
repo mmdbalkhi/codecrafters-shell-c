dceb849cad821b296b796a8e38a202e45462182f
Komeil Parseh <ahmdparsh129@gmail.com>
2025-01-27 22:36:24 +0330

    invalid_command

--- app/main.c
+++ app/main.c
@@ -1,14 +1,68 @@
+#define COMMAND_COUNT (sizeof(commands) / sizeof(commands[0]))
+
 #include <stdio.h>
+#include <string.h>
+
+typedef struct {
+  const char *name;
+  const char *description;
+  int min_args;
+  int max_args;
+  void (*execute)(char **args);
+} Command;
+
+void cmd_cd(char **args) { printf("Changing directory to: %s\n", args[1]); }
+
+void cmd_ls(char **args) { printf("Listing directory contents.\n"); }
+
+void cmd_exit(char **args) { printf("Exiting shell.\n"); }
+
+Command commands[] = {{"cd", "Change directory", 1, 1, cmd_cd},
+                      {"ls", "List directory contents", 0, 0, cmd_ls},
+                      {"exit", "Exit the shell", 0, 0, cmd_exit}};
+
+void execute_command(const char *input) {
+  char *args[10];
+  char input_copy[100];
+  strcpy(input_copy, input);
+
+  int argc = 0;
+  char *token = strtok(input_copy, " ");
+  while (token != NULL && argc < 10) {
+    args[argc++] = token;
+    token = strtok(NULL, " ");
+  }
+  args[argc] = NULL;
+
+  for (int i = 0; i < COMMAND_COUNT; i++) {
+    if (strcmp(args[0], commands[i].name) == 0) {
+      if (argc - 1 >= commands[i].min_args &&
+          argc - 1 <= commands[i].max_args) {
+        commands[i].execute(args);
+        return;
+      } else {
+        printf("Usage: %s\n", commands[i].description);
+        return;
+      }
+    }
+  }
+
+  printf("%s: command not found\n", args[0]);
+}
 
 int main() {
   // Flush after every printf
   setbuf(stdout, NULL);
 
-  // Uncomment this block to pass the first stage
-  // printf("$ ");
-
-  // Wait for user input
   char input[100];
-  fgets(input, 100, stdin);
+
+  while (1) {
+    printf("$ ");
+    fgets(input, sizeof(input), stdin);
+    input[strcspn(input, "\n")] = 0;
+    if (strlen(input) > 0) {
+      execute_command(input);
+    }
+  }
   return 0;
 }
